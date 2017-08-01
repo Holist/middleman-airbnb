7d236fad7e58ebd7627103c2c989de7103894a09
Romain Neuville <neuvilleromain@gmail.com>
2017-08-01 09:03:10 +0200

    finished home and team page

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |flat|
+  proxy "/flats/#{flat[0]}.html", "/flats/show.html", locals: { owner: flat[0] }, ignore: true
+end
